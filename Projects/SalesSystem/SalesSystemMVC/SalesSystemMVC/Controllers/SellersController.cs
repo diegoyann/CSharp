f73393ca522152074a2843c8f8dd88e261dbe2a8
unknown <diegoyann19@gmail.com>
2022-08-26 11:07:46 -0300

    Simple create form

--- Projects/SalesSystem/SalesSystemMVC/SalesSystemMVC/Controllers/SellersController.cs
+++ Projects/SalesSystem/SalesSystemMVC/SalesSystemMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesSystemMVC.Models;
 using SalesSystemMVC.Services;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,20 @@ namespace SalesSystemMVC.Controllers
 			var list = _sellerService.FindAll();
 
 			return View(list);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
+
 		}
 	}
 }
