48eb36e1ffdf2c5735a27eed7bb0b5220fd49d2b
unknown <diegoyann19@gmail.com>
2022-08-26 10:50:37 -0300

    Editing the Seller Service and creating the basic FindAll

---
+++ Projects/SalesSystem/SalesSystemMVC/SalesSystemMVC/Services/SellerService.cs
@@ -0,0 +1,26 @@
+﻿
+using SalesSystemMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesSystemMVC.Models;
+
+namespace SalesSystemMVC.Services
+{
+	public class SellerService
+	{
+
+		private readonly SalesSystemMVCContext _context;
+
+		public SellerService(SalesSystemMVCContext context)
+		{
+			_context = context;
+		}
+
+		public List<Seller> FindAll()
+		{
+			return _context.Seller.ToList();
+		}
+	}
+}
